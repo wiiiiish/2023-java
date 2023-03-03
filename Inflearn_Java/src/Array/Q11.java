bb0dda6ac667adb0c4e8f5230ff88c2aa761a319
wiiiiish <a90614@naver.com>
2023-03-03 16:59:13 +0900

    Q11 -RuntimeError

--- Inflearn_Java/src/Array/Q11.java
+++ Inflearn_Java/src/Array/Q11.java
@@ -1,15 +1,29 @@
 package Array;
 
-import java.util.Arrays;
-import java.util.Iterator;
 import java.util.Scanner;
 
 public class Q11 {
 	
-	public int solution (int n, int m, int[][] arr) {
+	public int solution (int n, int[][] arr) {
 		
 		int ans = 0; // 결과
+		int max = 0; // 같은 반 최댓값
+		int cnt = 0;
 		
+		for (int i=1; i<=n; i++) {
+			cnt = 0;
+			for (int k=1; k<=5; k++) {
+				for (int j=1; j<=n; j++) {
+					if (arr[i][k] == arr[j][k]) {
+						cnt++;
+					}
+				}
+			}
+			if (max < cnt) {
+				max = cnt;
+				ans = i; // 학생 번호
+			}
+		}
 		
 		return ans;
 	}
@@ -19,17 +33,15 @@ public class Q11 {
 		Scanner sc = new Scanner(System.in);
 		
 		int n = sc.nextInt(); // 입력받은 학생수
-		int m = 5; // 5반까지
-		int[][] arr = new int[n][5];
+		int[][] arr = new int[n+1][6];
 		
-		for (int i=0; i<n; i++) {
-			for (int j=0; j<m; j++) {
+		for (int i=1; i<=n; i++) {
+			for (int j=1; j<=5; j++) {
 				arr[i][j] = sc.nextInt();
 			}
 		}
 		
-		System.out.println(T.solution(n, m, arr));
-		
-		
+		System.out.println(T.solution(n, arr));
+
 	}
 }
