4a72c312cd9831d120365b36d6d7b23174154f2d
wiiiiish <a90614@naver.com>
2023-03-03 15:49:46 +0900

    Q11 -ing

--- Inflearn_Java/src/Array/Q11.java
+++ Inflearn_Java/src/Array/Q11.java
@@ -1,10 +1,35 @@
 package Array;
 
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.Scanner;
+
 public class Q11 {
 	
-	
+	public int solution (int n, int m, int[][] arr) {
+		
+		int ans = 0;
+		
+		
+		return ans;
+	}
 	
 	public static void main(String[] args) {
-		System.out.print("test1");
+		Q11 T = new Q11();
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt(); // 입력받은 학생수
+		int m = 5; // 5반까지
+		int[][] arr = new int[n][5];
+		
+		for (int i=0; i<n; i++) {
+			for (int j=0; j<m; j++) {
+				arr[i][j] = sc.nextInt();
+			}
+		}
+		
+		System.out.println(T.solution(n, m, arr));
+		
+		
 	}
 }
