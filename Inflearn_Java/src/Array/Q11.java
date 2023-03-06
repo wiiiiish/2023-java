bcba9e3a6f51afe85afec7a1bacf6301d5d0fb60
wiiiiish <a90614@naver.com>
2023-03-06 11:17:12 +0900

    Update Q11.java

--- Inflearn_Java/src/Array/Q11.java
+++ Inflearn_Java/src/Array/Q11.java
@@ -10,18 +10,20 @@ public class Q11 {
 		int max = 0; // 같은 반 최댓값
 		int cnt = 0; // count
 		
-		for (int i=1; i<=n; i++) {
+		for (int i=0; i<n; i++) {
 			cnt = 0;
-			for (int k=1; k<=5; k++) {
-				for (int j=1; j<=n; j++) {
-					if (arr[i][k] == arr[j][k]) {
-						cnt++;
+			for (int j=0; j<n; j++) {
+				if (i!=j) {
+					for (int k=0; k<5; k++) {
+						if (arr[i][k] == arr[j][k]) {
+							cnt++;
+						}
 					}
 				}
 			}
 			if (max < cnt) {
 				max = cnt;
-				ans = i; // 학생 번호
+				ans = i+1; // 학생 번호
 			}
 		}
 		
@@ -33,10 +35,10 @@ public class Q11 {
 		Scanner sc = new Scanner(System.in);
 		
 		int n = sc.nextInt(); // 입력받은 학생수
-		int[][] arr = new int[n+1][6];
+		int[][] arr = new int[n][5];
 		
-		for (int i=1; i<=n; i++) {
-			for (int j=1; j<=5; j++) {
+		for (int i=0; i<n; i++) {
+			for (int j=0; j<5; j++) {
 				arr[i][j] = sc.nextInt();
 			}
 		}
